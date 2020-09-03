23176139e23a10ba47bb3622da2b1d0ded5b2a1a
Francois PELLEGRINI <francois.pellegrini@labri.fr>
2020-09-03 11:04:50 +0200

    Comment fix

--- src/libscotch/hall_order_hx.c
+++ src/libscotch/hall_order_hx.c
@@ -8,13 +8,13 @@
 ** use, modify and/or redistribute the software under the terms of the
 ** CeCILL-C license as circulated by CEA, CNRS and INRIA at the following
 ** URL: "http://www.cecill.info".
-** 
+**
 ** As a counterpart to the access to the source code and rights to copy,
 ** modify and redistribute granted by the license, users are provided
 ** only with a limited warranty and the software's author, the holder of
 ** the economic rights, and the successive licensors have only limited
 ** liability.
-** 
+**
 ** In this respect, the user's attention is drawn to the risks associated
 ** with loading, using, modifying and/or developing or reproducing the
 ** software by the user in light of its specific status of free software,
@@ -25,7 +25,7 @@
 ** their requirements in conditions enabling the security of their
 ** systems and/or data to be ensured and, more generally, to use and
 ** operate it in the same conditions as regards security.
-** 
+**
 ** The fact that you are presently reading this means that you have had
 ** knowledge of the CeCILL-C license and that you accept its terms.
 */
@@ -213,7 +213,7 @@ const float                 fillrat)
           for (k = secntax[i]; secntax[k] != ~0; k = secntax[k]) ; /* Find last node      */
           secntax[k] = secntax[j];                /* Append father list to node list      */
         }
-        secntax[j] = i;                           /* Now he is a secondary variable of it */
+        secntax[j] = i;                           /* Now it is a secondary variable of it */
         if (frsttax[j] == i) {                    /* If node is first son of father       */
           if (frsttax[i] == ~0)                   /* If node has no sons                  */
             frsttax[j] = nexttax[i];              /* First son is now next node           */
