528863b79803339d473403740509f98f7f4ba063
Francois PELLEGRINI <francois.pellegrini@labri.fr>
2021-04-04 09:28:32 +0200

    Updated script "analysis.sh" for SonarScanner 4.6

--- ci/analysis.sh
+++ ci/analysis.sh
@@ -14,6 +14,11 @@ cppcheck -v --max-configs=1 --language=c ${CPPCHECK_DEFINITIONS_VM:---platform=n
 
 rats -w 3 --xml `cat filelist.txt` > scotch-rats.xml
 
+# sonar.projectKey=tadaam:scotch:gitlab:$CI_PROJECT_NAMESPACE:$CI_COMMIT_REF_NAME
+# sonar.projectKey=tadaam:scotch:gitlab:$CI_PROJECT_NAMESPACE
+# sonar.branch.name=$CI_COMMIT_REF_NAME
+# sonar.exclusions=build/**,cmake_modules/**,doc/**,tools/**,**/*.xml
+# "sonar.lang.patterns.c++=**/*.cpp,**/*.hpp" : needed so that C files are not considered as C++ files
 cat > sonar-project.properties << EOF
 sonar.host.url=https://sonarqube.inria.fr/sonarqube
 sonar.login=$SONARQUBE_LOGIN
@@ -21,14 +26,16 @@ sonar.links.homepage=$CI_PROJECT_URL
 sonar.links.scm=$CI_REPOSITORY_URL
 sonar.links.ci=https://gitlab.inria.fr/$CI_PROJECT_NAMESPACE/scotch/pipelines
 sonar.links.issue=https://gitlab.inria.fr/$CI_PROJECT_NAMESPACE/scotch/issues
-sonar.projectKey=tadaam:scotch:gitlab:$CI_PROJECT_NAMESPACE:$CI_COMMIT_REF_NAME
+sonar.projectKey=tadaam:scotch:gitlab:$CI_PROJECT_NAMESPACE
+sonar.branch.name=$CI_COMMIT_REF_NAME
 sonar.projectDescription=Package for graph and mesh/hypergraph partitioning, graph clustering, and sparse matrix ordering.
-sonar.projectVersion=6.0
-sonar.language=c
+sonar.projectVersion=6.1
 sonar.sourceEncoding=UTF-8
 sonar.sources=include,src/check,src/esmumps,src/libscotch,src/libscotchmetis,src/misc,src/scotch
-sonar.exclusions=src/libscotch/dummysizes.c,src/libscotch/parser_ll.c,src/libscotch/parser_yy.c,src/libscotch/last_resort/parser_ll.c,src/libscotch/last_resort/parser_yy.c
-sonar.c.includeDirectories=$(echo | gcc -E -Wp,-v - 2>&1 | grep "^ " | tr '\n' ',')include,src/scotch,src/misc,src/libscotch,src/esmumps,src/libscotchmetis
+sonar.lang.patterns.c++=**/*.cpp,**/*.hpp
+sonar.lang.patterns.c=**/*.c,**/*.h
+sonar.exclusions=src/libscotch/dummysizes.c,src/libscotch/parser_ll.c,src/libscotch/parser_yy.c,src/libscotch/last_resort/**
+sonar.c.includeDirectories=$(echo | gcc -E -Wp,-v - 2>&1 | grep "^ " | tr '\n' ',')include,src/scotch,src/misc,src/libscotch,src/esmumps,src/libscotchmetis,/usr/include/openmpi,/usr/include/mpich
 sonar.c.errorRecoveryEnabled=true
 sonar.c.gcc.charset=UTF-8
 sonar.c.gcc.regex=(?<file>.*):(?<line>[0-9]+):[0-9]+:\\\x20warning:\\\x20(?<message>.*)\\\x20\\\[(?<id>.*)\\\]
