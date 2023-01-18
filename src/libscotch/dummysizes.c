b1162c30ddf7152fa3c65d1459ca00565ac4a178
Francois PELLEGRINI <francois.pellegrini@labri.fr>
2023-01-18 09:48:40 +0100

    Comment fix

--- src/libscotch/dummysizes.c
+++ src/libscotch/dummysizes.c
@@ -1,4 +1,4 @@
-/* Copyright 2004,2007-2010,2012,2014,2018,2019,2021 Universite de Bordeaux, INRIA & CNRS
+/* Copyright 2004,2007-2010,2012,2014,2018,2019,2021 IPB, Universite de Bordeaux, INRIA & CNRS
 **
 ** This file is part of the Scotch software package for static mapping,
 ** graph partitioning and sparse matrix ordering.
