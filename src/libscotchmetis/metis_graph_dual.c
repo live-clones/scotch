a2dc0d7e6368de78f23a7524b299ab7f52e5eeb9
Francois PELLEGRINI <francois.pellegrini@labri.fr>
2023-01-21 21:39:13 +0100

    Remove unused source file marker definitions in libScotchMeTiS [report: R. Brzegowy & P. Jolivet]

--- src/libscotchmetis/metis_graph_dual.c
+++ src/libscotchmetis/metis_graph_dual.c
@@ -1,4 +1,4 @@
-/* Copyright 2020,2021 IPB, Universite de Bordeaux, INRIA & CNRS
+/* Copyright 2020,2021,2023 IPB, Universite de Bordeaux, INRIA & CNRS
 **
 ** This file is part of the Scotch software package for static mapping,
 ** graph partitioning and sparse matrix ordering.
@@ -43,14 +43,14 @@
 /**                                                        **/
 /**   DATES      : # Version 6.1  : from : 01 sep 2020     **/
 /**                                 to   : 28 may 2021     **/
+/**                # Version 7.0  : from : 21 jan 2023     **/
+/**                                 to   : 21 jan 2023     **/
 /**                                                        **/
 /************************************************************/
 
 /*
 **  The defines and includes.
 */
-
-#define LIBRARY
 
 #include "module.h"
 #include "common.h"
--- src/libscotchmetis/metis_options_f.c
+++ src/libscotchmetis/metis_options_f.c
@@ -1,4 +1,4 @@
-/* Copyright 2021 IPB, Universite de Bordeaux, INRIA & CNRS
+/* Copyright 2021,2023 IPB, Universite de Bordeaux, INRIA & CNRS
 **
 ** This file is part of the Scotch software package for static mapping,
 ** graph partitioning and sparse matrix ordering.
@@ -42,14 +42,14 @@
 /**                                                        **/
 /**   DATES      : # Version 6.1  : from : 20 jun 2021     **/
 /**                                 to   : 20 jun 2021     **/
+/**                # Version 7.0  : from : 21 jan 2023     **/
+/**                                 to   : 21 jan 2023     **/
 /**                                                        **/
 /************************************************************/
 
 /*
 **  The defines and includes.
 */
-
-#define LIBRARY
 
 #include "module.h"
 #include "common.h"
