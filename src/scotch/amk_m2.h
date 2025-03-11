63ecd5702ac2e875ff118abe7ac96f298ff9ae46
Clément Barthélemy <clement.barthelemy@inria.fr>
2025-03-11 23:34:11 +0100

    Bugfix: make C_domnBipart() declaration consistent in amk_m2.h [report D. Parsons]

--- src/scotch/amk_m2.h
+++ src/scotch/amk_m2.h
@@ -1,4 +1,4 @@
-/* Copyright 2004,2007,2008,2014,2019,2024 IPB, Universite de Bordeaux, INRIA & CNRS
+/* Copyright 2004,2007,2008,2014,2019,2024,2025 IPB, Universite de Bordeaux, INRIA & CNRS
 **
 ** This file is part of the Scotch software package for static mapping,
 ** graph partitioning and sparse matrix ordering.
@@ -55,7 +55,7 @@
 /**                # Version 6.0  : from : 12 nov 2014     **/
 /**                                 to   : 16 apr 2019     **/
 /**                # Version 7.0  : from : 10 sep 2024     **/
-/**                                 to   : 10 sep 2024     **/
+/**                                 to   : 11 mar 2025     **/
 /**                                                        **/
 /************************************************************/
 
@@ -91,7 +91,7 @@ typedef int (* DomnBipartFunc) (const ArchMesh2 * const, const ArchMesh2Dom * co
 **  The function prototypes.
 */
 
-void                        C_domnBipart        (ArchMesh2 *, ArchMesh2Dom *, Anum, Anum *, Anum *, int (*) ());
+void                        C_domnBipart        (ArchMesh2 *, ArchMesh2Dom *, Anum, Anum *, Anum *, DomnBipartFunc);
 int                         C_domnBipartOne     (const ArchMesh2 * const, const ArchMesh2Dom * const, ArchMesh2Dom * restrict const, ArchMesh2Dom * restrict const);
 
 /*
