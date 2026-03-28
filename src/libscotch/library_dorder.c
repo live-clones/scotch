47150b3ccef0f43cb6afe4280f061c1aaff264a2
Francois PELLEGRINI <francois.pellegrini@u-bordeaux.fr>
2026-03-28 22:41:45 +0100

    Style fix

--- src/libscotch/library_dorder.c
+++ src/libscotch/library_dorder.c
@@ -65,13 +65,13 @@
 /*                                       */
 /*****************************************/
 
-/*+ This routine reserves a memory area
-*** of a size sufficient to store a
-*** SCOTCH_Dordering structure.
-*** It returns:
-*** - !NULL  : if the allocation succeeded.
-*** - NULL   : on error.
-+*/
+/* This routine reserves a memory area
+** of a size sufficient to store a
+** SCOTCH_Dordering structure.
+** It returns:
+** - !NULL  : if the allocation succeeded.
+** - NULL   : on error.
+*/
 
 SCOTCH_Dordering *
 SCOTCH_dorderAlloc ()
@@ -79,11 +79,11 @@ SCOTCH_dorderAlloc ()
   return ((SCOTCH_Dordering *) memAlloc (sizeof (SCOTCH_Dordering)));
 }
 
-/*+ This routine returns the size, in bytes,
-*** of a SCOTCH_Dordering structure.
-*** It returns:
-*** - > 0  : in all cases.
-+*/
+/* This routine returns the size, in bytes,
+** of a SCOTCH_Dordering structure.
+** It returns:
+** - > 0  : in all cases.
+*/
 
 int
 SCOTCH_dorderSizeof ()
