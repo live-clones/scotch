d3bdd68d97721d68753a9c0cb360a7de16096776
Francois PELLEGRINI <francois.pellegrini@labri.fr>
2026-07-08 20:14:35 +0200

    Comment fix

--- src/libscotch/wgraph_store.c
+++ src/libscotch/wgraph_store.c
@@ -92,7 +92,7 @@ WgraphStore * const         storptr)
 
 /* This routine frees a save structure.
 ** It returns:
-** - VOID  : in all cases.
+** - void  : in all cases.
 */
 
 void
@@ -108,7 +108,7 @@ WgraphStore * const         storptr)
 /* This routine saves partition data from the
 ** given active graph to the given save structure.
 ** It returns:
-** - VOID  : in all cases.
+** - void  : in all cases.
 */
 
 void
@@ -139,7 +139,7 @@ WgraphStore * const         storptr)
 /* This routine updates partition data of the
 ** given active graph, using the given save graph.
 ** It returns:
-** - VOID  : in all cases.
+** - void  : in all cases.
 */
 
 void
