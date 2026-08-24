adb2b644d1821c5e5f23fe059c8154d5817ac6fb
Clément Barthélemy <clement.barthelemy@inria.fr>
2026-08-24 21:18:05 +0200

    Bugfix: base treetab in `dorderTreeDist()`

--- src/libscotch/dorder_tree_dist.c
+++ src/libscotch/dorder_tree_dist.c
@@ -1,4 +1,4 @@
-/* Copyright 2007,2008,2023 IPB, Universite de Bordeaux, INRIA & CNRS
+/* Copyright 2007,2008,2023,2026 IPB, Universite de Bordeaux, INRIA & CNRS
 **
 ** This file is part of the Scotch software package for static mapping,
 ** graph partitioning and sparse matrix ordering.
@@ -41,7 +41,7 @@
 /**   DATES      : # Version 5.1  : from : 28 nov 2007     **/
 /**                                 to   : 09 may 2008     **/
 /**                # Version 7.0  : from : 17 jan 2023     **/
-/**                                 to   : 17 jan 2023     **/
+/**                                 to   : 25 aug 2026     **/
 /**                                                        **/
 /************************************************************/
 
@@ -97,7 +97,9 @@ const Dorder * restrict const ordeptr)
 ** distributed part of the distributed structure of
 ** the given distributed ordering. The two array
 ** pointers which must be passed should both point to
-** arrays of size dorderCblkDist().
+** arrays of size dorderCblkDist(). Tree node indices
+** are based, while the father index of fatherless
+** nodes (tree roots) is always -1.
 ** It returns:
 ** - 0   : if the distributed tree structure could be computed.
 ** - !0  : on error.
@@ -106,7 +108,6 @@ const Dorder * restrict const ordeptr)
 int
 dorderTreeDist (
 const Dorder * restrict const ordeptr,
-const Dgraph * restrict const grafptr,
 Gnum * restrict const         treeglbtab,
 Gnum * restrict const         sizeglbtab)
 {
@@ -120,14 +121,18 @@ Gnum * restrict const         sizeglbtab)
   Gnum                        dblkglbtmp;
   int * restrict              dblkcnttab;
   int * restrict              dblkdsptab;
-  int * restrict              cblkdsptab;
+  int *                       cblkdsptab;         /* TRICK: continues dblkcnttab [norestrict]   */
   Gnum                        cblkglbtmp;
   Gnum * restrict             srt1glbtab;
   Gnum * restrict             srt2glbtab;
+  Gnum * restrict             sizeglbtax;         /* Based access to sizeglbtab                 */
+  Gnum * restrict             treeglbtax;         /* Based access to treeglbtab                 */
   int                         procglbnbr;
   int                         procnum;
   Gnum                        reduloctab[3];
   Gnum                        reduglbtab[3];
+
+  const Gnum                        baseval = ordeptr->baseval;
 
   for (linklocptr = ordeptr->linkdat.nextptr, dblklocnbr = 0; /* For all nodes in local ordering structure */
        linklocptr != &ordeptr->linkdat; linklocptr = linklocptr->nextptr) {
@@ -226,10 +231,10 @@ Gnum * restrict const         sizeglbtab)
 
     cblklocptr = (DorderCblk *) linklocptr;       /* TRICK: FIRST                    */
     if (cblklocptr->cblknum.proclocnum == ordeptr->proclocnum) { /* If node is local */
-      dataloctab[4 * dblklocnum]     = cblkdsptab[ordeptr->proclocnum] + cblklocptr->cblknum.cblklocnum;
-      dataloctab[4 * dblklocnum + 1] = cblklocptr->ordeglbval;
-      dataloctab[4 * dblklocnum + 2] = cblkdsptab[cblklocptr->fathnum.proclocnum] + cblklocptr->fathnum.cblklocnum;
-      dataloctab[4 * dblklocnum + 3] = cblklocptr->vnodglbnbr;
+      dataloctab[4 * dblklocnum]     = cblkdsptab[ordeptr->proclocnum] + cblklocptr->cblknum.cblklocnum; /* Global index of column block      */
+      dataloctab[4 * dblklocnum + 1] = cblklocptr->ordeglbval; /* Start inverse permutation index for column block                            */
+      dataloctab[4 * dblklocnum + 2] = cblkdsptab[cblklocptr->fathnum.proclocnum] + cblklocptr->fathnum.cblklocnum; /* Global index of father */
+      dataloctab[4 * dblklocnum + 3] = cblklocptr->vnodglbnbr; /* Size of column block subtree */
       dblklocnum ++;
     }
   }
@@ -239,19 +244,19 @@ Gnum * restrict const         sizeglbtab)
   }
 
   for (dblkglbnum = 0; dblkglbnum < dblkglbnbr; dblkglbnum ++) {
-    srt1glbtab[2 * dblkglbnum]     = dataglbtab[4 * dblkglbnum + 1];
-    srt1glbtab[2 * dblkglbnum + 1] = dataglbtab[4 * dblkglbnum];
+    srt1glbtab[2 * dblkglbnum]     = dataglbtab[4 * dblkglbnum + 1]; /* Start inverse permutation index for column block */
+    srt1glbtab[2 * dblkglbnum + 1] = dataglbtab[4 * dblkglbnum]; /* Global index of column block                         */
   }
   intSort2asc2 (srt1glbtab, dblkglbnbr);          /* Sort nodes by ascending inverse start index to get permutation of column block indices */
   for (dblkglbnum = 0; dblkglbnum < dblkglbnbr; dblkglbnum ++) {
-    srt1glbtab[2 * dblkglbnum]     = srt1glbtab[2 * dblkglbnum + 1];
-    srt1glbtab[2 * dblkglbnum + 1] = dblkglbnum;
+    srt1glbtab[2 * dblkglbnum]     = srt1glbtab[2 * dblkglbnum + 1]; /* Global index of column block */
+    srt1glbtab[2 * dblkglbnum + 1] = dblkglbnum + baseval; /* Base global column block numbers       */
   }
   intSort2asc2 (srt1glbtab, dblkglbnbr);          /* Sort nodes by ascending column block index to match with the ones of dataglbtab */
 
   for (dblkglbnum = 0; dblkglbnum < dblkglbnbr; dblkglbnum ++) {
-    srt2glbtab[2 * dblkglbnum]     = dataglbtab[4 * dblkglbnum + 2];
-    srt2glbtab[2 * dblkglbnum + 1] = dblkglbnum;
+    srt2glbtab[2 * dblkglbnum]     = dataglbtab[4 * dblkglbnum + 2]; /* Global index of father */
+    srt2glbtab[2 * dblkglbnum + 1] = dblkglbnum;  /* Index of column block data                */
   }
   intSort2asc2 (srt2glbtab, dblkglbnbr);          /* Sort father indices by ascending column block indices */
 #ifdef SCOTCH_DEBUG_DORDER2
@@ -268,7 +273,7 @@ Gnum * restrict const         sizeglbtab)
 #endif /* SCOTCH_DEBUG_DORDER2 */
   for (dblkglbnum = 1, dblkglbtmp = 0; dblkglbnum < dblkglbnbr; ) { /* Replace in block data the father column block indices by the new permuted indices */
     if (srt2glbtab[2 * dblkglbnum] == srt1glbtab[2 * dblkglbtmp])
-      dataglbtab[4 * srt2glbtab[2 * (dblkglbnum ++) + 1] + 2] = srt1glbtab[2 * dblkglbtmp + 1];
+      dataglbtab[4 * srt2glbtab[2 * (dblkglbnum ++) + 1] + 2] = srt1glbtab[2 * dblkglbtmp + 1]; /* Propagate based global column block numbers */
     else {
 #ifdef SCOTCH_DEBUG_DORDER2
       if ((srt2glbtab[2 * dblkglbnum] < srt1glbtab[2 * dblkglbtmp]) || /* If column block index not found in table */
@@ -283,10 +288,12 @@ Gnum * restrict const         sizeglbtab)
   }
 
   for (dblkglbnum = 0; dblkglbnum < dblkglbnbr; dblkglbnum ++) {
-    srt2glbtab[2 * dblkglbnum]     = dataglbtab[4 * dblkglbnum];
-    srt2glbtab[2 * dblkglbnum + 1] = dblkglbnum;
+    srt2glbtab[2 * dblkglbnum]     = dataglbtab[4 * dblkglbnum]; /* Global index of column block */
+    srt2glbtab[2 * dblkglbnum + 1] = dblkglbnum;  /* Index of column block data                  */
   }
   intSort2asc2 (srt2glbtab, dblkglbnbr);          /* Sort father indices by ascending column block indices */
+  treeglbtax = treeglbtab - baseval;
+  sizeglbtax = sizeglbtab - baseval;
   for (dblkglbnum = 0; dblkglbnum < dblkglbnbr; dblkglbnum ++) {
 #ifdef SCOTCH_DEBUG_DORDER2
     if (srt1glbtab[2 * dblkglbnum] != srt2glbtab[2 * dblkglbnum]) {
@@ -295,8 +302,8 @@ Gnum * restrict const         sizeglbtab)
       return (1);
     }
 #endif /* SCOTCH_DEBUG_DORDER2 */
-    treeglbtab[srt1glbtab[2 * dblkglbnum + 1]] = dataglbtab[4 * srt2glbtab[2 * dblkglbnum + 1] + 2];
-    sizeglbtab[srt1glbtab[2 * dblkglbnum + 1]] = dataglbtab[4 * srt2glbtab[2 * dblkglbnum + 1] + 3];
+    treeglbtax[srt1glbtab[2 * dblkglbnum + 1]] = dataglbtab[4 * srt2glbtab[2 * dblkglbnum + 1] + 2]; /* Based column block numbers   */
+    sizeglbtax[srt1glbtab[2 * dblkglbnum + 1]] = dataglbtab[4 * srt2glbtab[2 * dblkglbnum + 1] + 3]; /* Size of column block subtree */
   }
 
   memFree (dblkcnttab);                           /* Free group leader */
